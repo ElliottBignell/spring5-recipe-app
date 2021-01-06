ab78343229e029e530ae732df7a53679221ec485
Elliott Bignell <elliott.bignell@gmail.com>
2021-01-06 17:00:07 +0100

    Added repositories and data.sql

--- src/main/java/bignell/elliott/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/bignell/elliott/spring5recipeapp/controllers/IndexController.java
@@ -1,2 +1,16 @@
-package bignell.elliott.spring5recipeapp.controllers;public class IndexController {
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 6/1/17.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
 }
