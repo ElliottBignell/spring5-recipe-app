933efc304a67222f85a4484c49a5f06e779b6dce
Elliott Bignell <elliott.bignell@gmail.com>
2021-01-08 14:27:17 +0100

    Added creipe service test

---
+++ src/test/java/bignell/elliott/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,19 @@
+package bignell.elliott.spring5recipeapp.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CategoryTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
